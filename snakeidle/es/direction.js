470d9bbd23c915e1a3e21a9bb74f778ad64c2d18
Tunvior <tunvior@gmail.com>
2017-06-05 15:10:35 +0200

    removed transplier files

--- snakeidle/es/direction.js
+++
@@ -1,6 +0,0 @@
-var Direction = {
-  UP: 0,
-  RIGHT: 1,
-  DOWN: 2,
-  LEFT: 3,
-};
--- snakeidle/es/state.js
+++
@@ -1,8 +0,0 @@
-var State = {
-  FREE: 0,
-  FRUIT: 1,
-  SNAKE: 2,
-  HEAD: 3,
-  GOLDEN_FRUIT: 4,
-  PURPLE_FRUIT: 5,
-};
